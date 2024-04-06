eb981838d452bdfe6f3fac7d1146fb161193007a
Collins C. A <contactmrc4@gmail.com>
2024-04-06 23:43:45 +0200

    Created Reword class

---
+++ Reward.cs
@@ -0,0 +1,14 @@
+﻿namespace schoolProject
+{
+    public class Reward
+    {
+        public Position rewardPosition { get; set; }
+        public int points { get; set; }
+
+        public Reward(Position rewardPosition, int points) 
+        {
+            this.rewardPosition = rewardPosition;
+            this.points = points;
+        }
+    }
+}

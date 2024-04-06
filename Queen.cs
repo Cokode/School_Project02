8906ee72f4e0928ceb12e6e0c560ba1da3ba1017
Collins C. A <contactmrc4@gmail.com>
2024-04-06 23:51:02 +0200

    created Queen class

---
+++ Queen.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Security;
+
+namespace schoolProject
+{
+    public class Queen
+    {
+        public Position queenPosition;
+        public int points;
+
+        public Queen(Position queenPosition, int points)
+        {
+            this.queenPosition = queenPosition;
+            this.points = points;
+        }
+
+        public override string ToString()
+        {
+            return queenPosition.ToString();
+        } // from the window 
+
+        
+    }
+}

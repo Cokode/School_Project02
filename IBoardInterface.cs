3b1699320dd15178000b45c6773c7582cd1df126
Collins C. A <contactmrc4@gmail.com>
2024-04-06 23:44:19 +0200

    Created Interface for BordLogic class

---
+++ IBoardInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schoolProject
+{
+    public interface IBoardInterface
+    {
+        // Hero hero, char[,] board, Queen queen, Reward reward
+        public void LoadBoard(char[,] gameBaord);
+        public void LoadWalls(Wall wall, char[,] gameBaord);
+
+        public bool CheckPosition(Wall wall, char[,] gameBoard);
+
+
+    }
+}

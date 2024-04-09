26c499a328dcd73d9315c3baa67795b2488b1d8f
Collins C. A <contactmrc4@gmail.com>
2024-04-09 20:45:52 +0200

    Refactored to three properties, wallType, isAWall, Wallposition

--- Wall.cs
+++ Wall.cs
@@ -2,27 +2,18 @@
 {
     public class Wall
     {
-        public bool isUp {  get; set; }
-        public bool isDown { get; set; }
-        public bool isLeft { get; set; }
-        public bool isRight { get; set; }
+        public char wallType {  get; set; }
+        public bool isAWall { get; set; }
         public Position? WallPosition { get; set; }
 
         public Wall()
         {
-            isUp = false;
-            isDown = false;
-            isLeft = false;
-            isRight = false;
-            WallPosition = null;
         }
 
-        public Wall(bool isUp,bool isDown, bool isLeft, bool isRight, Position position)
+        public Wall(char wallType, bool isAWall, Position position)
         {
-            this.isUp = isUp;
-            this.isDown = isDown;
-            this.isLeft = isLeft;
-            this.isRight = isRight;
+            this.wallType = wallType;
+            this.isAWall = isAWall;
             this.WallPosition = position;
         }
          

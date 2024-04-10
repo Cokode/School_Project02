4d5eb933b6f6a8932f86ee4f2069340bb8bec112
Collins C. A <contactmrc4@gmail.com>
2024-04-10 19:17:20 +0200

    Update: Added 'isHaveReward' property to Reward class constructor in Reward.cs.

--- Reward.cs
+++ Reward.cs
@@ -4,11 +4,13 @@
     {
         public Position rewardPosition { get; set; }
         public int points { get; set; }
+        public bool isHaveReward { get; set; }
 
-        public Reward(Position rewardPosition, int points) 
+        public Reward(Position rewardPosition, int points, bool isHaveReward) 
         {
             this.rewardPosition = rewardPosition;
             this.points = points;
+            this.isHaveReward = isHaveReward;
         }
     }
 }
